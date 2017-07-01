9eadf6de1b5e8e57fd57d25e114d4a70213f56af
Moshe Solomon <udeleng@gmail.com>
2017-07-01 16:55:25 -0700

    Refactored behavior into TitleBar, AddPlayer, and Player components

--- client/main.js
+++ client/main.js
@@ -4,51 +4,26 @@ import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
 
 import { Players } from './../imports/api/players';
+import TitleBar from './../imports/ui/TitleBar';
+import AddPlayer from './../imports/ui/AddPlayer';
+import Player from './../imports/ui/Player';
 
 const renderPlayers = ( playersList ) => {
     return playersList.map(( player ) => {
-        return (
-            <p key={player._id}>
-                { player.name } has { player.score } point(s).
-                <button onClick={() => {
-                    Players.update(player._id, {$inc: {score: -1}})
-                }}>-1</button>
-                <button onClick={() => {
-                    Players.update(player._id, {$inc: {score: 1}})
-                }}>+1</button>
-                <button onClick={() => Players.remove(player._id)}>X</button>
-            </p>
-        );
+        return <Player key={player._id} player={player} />;
     });
-};
-
-const handleSubmit = (e) => {
-    let playerName = e.target.playerName.value;
-    e.preventDefault();
-    if (playerName) {
-        e.target.playerName.value = '';
-        Players.insert({
-            name: playerName,
-            score: 0
-        });
-    }
 };
 
 Meteor.startup(() => {
     Tracker.autorun(() => {
         let players = Players.find().fetch();
         let title = 'Score Keep';
-        let name = 'Moshe';
+        let subtitle = 'Create by Moshe';
         let jsx = (
             <div>
-                <h1>{ title }</h1>
-                <p>Hello {name}!</p>
-                <p>This is my second p.</p>
+                <TitleBar title={title} subtitle={subtitle}/>
                 { renderPlayers( players ) }
-                <form onSubmit={handleSubmit}>
-                    <input type="text" name="playerName" placeholder="Player name"/>
-                    <button>Add Player</button>
-                </form>
+                <AddPlayer/>
             </div>
         );
         ReactDom.render(jsx, document.getElementById('app'));
---
+++ imports/ui/TitleBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class TitleBar extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.title}</h1>
+                <h3>{this.props.subtitle}</h3>
+            </div>
+        );
+    }
+}
+
+TitleBar.propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired
+};
+
+TitleBar.defaultProps = {
+    // title: 'Default title'
+}
