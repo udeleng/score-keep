862998f2b2a203849e2503f93544c63455e7fc20
Moshe Solomon <udeleng@gmail.com>
2017-07-02 00:52:49 -0700

    Refactored remaining code into PlayerList and App components

--- client/main.js
+++ client/main.js
@@ -4,28 +4,13 @@ import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
 
 import { Players } from './../imports/api/players';
-import TitleBar from './../imports/ui/TitleBar';
-import AddPlayer from './../imports/ui/AddPlayer';
-import Player from './../imports/ui/Player';
+import App from './../imports/ui/App';
 
-const renderPlayers = ( playersList ) => {
-    return playersList.map(( player ) => {
-        return <Player key={player._id} player={player} />;
-    });
-};
 
 Meteor.startup(() => {
     Tracker.autorun(() => {
-        let players = Players.find().fetch();
+        let players = Players.find({}, {sort: {score: -1}}).fetch();
         let title = 'Score Keep';
-        let subtitle = 'Create by Moshe';
-        let jsx = (
-            <div>
-                <TitleBar title={title} subtitle={subtitle}/>
-                { renderPlayers( players ) }
-                <AddPlayer/>
-            </div>
-        );
-        ReactDom.render(jsx, document.getElementById('app'));
+        ReactDom.render(<App title={title} players={players}/>, document.getElementById('app'));
     });
 });
--- imports/ui/TitleBar.js
+++ imports/ui/TitleBar.js
@@ -2,11 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class TitleBar extends React.Component {
+    renderSubtitle() {
+        if (this.props.subtitle) {
+            return <h3>{this.props.subtitle}</h3>
+        }
+    }
+
     render() {
         return (
             <div>
                 <h1>{this.props.title}</h1>
-                <h3>{this.props.subtitle}</h3>
+                {this.renderSubtitle()}
             </div>
         );
     }
@@ -14,7 +20,7 @@ export default class TitleBar extends React.Component {
 
 TitleBar.propTypes = {
     title: PropTypes.string.isRequired,
-    subtitle: PropTypes.string.isRequired
+    subtitle: PropTypes.string
 };
 
 TitleBar.defaultProps = {
